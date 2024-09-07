cf938d5c5cad461ad8e6cdbb2b0681ad731aca39
karun4242 <aletikarun786@gmail.com>
2024-09-07 09:13:10 +0800

    small changes1

--- src/test/java/pack/RacesList.java
+++ src/test/java/pack/RacesList.java
@@ -9,4 +9,62 @@ public class RacesList {
     int betAmount;
     int banker;
 
+
+    public int getBanker() {
+        return banker;
+    }
+
+    public void setBanker(int banker) {
+        this.banker = banker;
+    }
+
+    public String getBetrace() {
+        return betrace;
+    }
+
+    public void setBetrace(String betrace) {
+        this.betrace = betrace;
+    }
+
+    public String[] getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String[] venue) {
+        this.venue = venue;
+    }
+
+    public String getSelectionList() {
+        return selectionList;
+    }
+
+    public void setSelectionList(String selectionList) {
+        this.selectionList = selectionList;
+    }
+
+    public String getBetType() {
+        return betType;
+    }
+
+    public void setBetType(String betType) {
+        this.betType = betType;
+    }
+
+    public int getRace() {
+        return race;
+    }
+
+    public void setRace(int race) {
+        this.race = race;
+    }
+
+    public int getBetAmount() {
+        return betAmount;
+    }
+
+    public void setBetAmount(int betAmount) {
+        this.betAmount = betAmount;
+    }
+
+
 }
