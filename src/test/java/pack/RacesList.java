f7450ebf32e72642a53ff7b7262c1213192c7855
karun4242 <aletikarun786@gmail.com>
2024-09-07 08:40:33 +0800

    small changes

---
+++ src/test/java/pack/RacesList.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class RacesList {
+    String  betrace;
+    String [] venue;
+    String betType;
+    int race;
+    String selectionList;
+    int betAmount;
+    int banker;
+
+}
